bfbeccdb0ffc8086f85c4f05fc881f63af7201bb
Your Name <amirtaja@yahoo.com>
2023-03-04 20:47:43 +0330

    d

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('phone')->unique();
+            $table->string('username', 50)->unique();
+            $table->string('email', 50)->unique();
+            $table->string('phone', 50)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
