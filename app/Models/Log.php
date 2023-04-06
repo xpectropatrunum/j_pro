5d28b4a5b51edcb3912f11d63d79e4b8c6c4f7da
Your Name <amirtaja@yahoo.com>
2023-04-06 18:53:19 +0430

    f

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -43,7 +43,7 @@ class Log extends Model
             if(strtotime(date("Y-m-d H:i")) >= strtotime($this->date  . " ".env("MAX_WORKING_HOUR"))){
                 if (time() - strtotime($this->date  . " " . $end_time) > 0) {
                     if (strtotime($this->date . " " . $end_time) - strtotime($this->date . " " . $this->time) > 0) {
-                        $this->leave()->create([
+                        $this->leave()->updateOrCreate([
                             "note" => "",
                             "fee" => "",
                             "date" => $this->date  ,
