875b87588743da1d23b41f0be01e44f5c8e671a2
Amir <amirtaja@yahoo.com>
2023-04-21 19:53:39 +0430

    f

--- resources/views/admin/pages/leaves/index.blade.php
+++ resources/views/admin/pages/leaves/index.blade.php
@@ -87,7 +87,7 @@
                                             </td>
 
                                             <td>{{ number_format($item->fee ?: 0) }}</td>
-                                            <td><a href="#" data-toggle="tooltip" title="{{$item->note}}">{{ substr($item->note, 20) . ".." }}</a></td>
+                                            <td><a href="#" data-toggle="tooltip" title="{{$item->note}}">{{ mb_substr($item->note, 20) . ".." }}</a></td>
 
                                             {{--  <td class="project-actions">
                                   
