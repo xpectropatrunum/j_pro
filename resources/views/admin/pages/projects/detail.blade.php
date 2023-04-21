15091b0d73ea6ba668b431df8ce27fec350a8ace
Amir <amirtaja@yahoo.com>
2023-04-21 20:10:55 +0430

    f

--- resources/views/admin/pages/projects/detail.blade.php
+++ resources/views/admin/pages/projects/detail.blade.php
@@ -68,9 +68,9 @@
                                     foreach ($items as $k) {
                                         $time = $k->duration;
                                         if (isset($l[$k->user->id])) {
-                                            $l[$k->user->id]->duration += $item->duration_seconds;
+                                            $l[$k->user->id]->duration += $k->duration_seconds;
                                         } else {
-                                            $l[$k->user->id] = (object)['user' => $k->user, 'duration' => $item->duration_seconds];
+                                            $l[$k->user->id] = (object)['user' => $k->user, 'duration' => $k->duration_seconds];
                                         }
                                     }
                                  
